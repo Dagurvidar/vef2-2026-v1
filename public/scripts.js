a41b8ce19268a09843abb65f84374d9059a19900
Dagur Ingi <daguringividar@gmail.com>
2026-01-21 17:19:43 +0000

    byrjað á client-side js

--- public/scripts.js
+++ public/scripts.js
@@ -1,1 +1,36 @@
 /* útfæra */
+//finnur elements með klasa counter og correct
+const correctElement = document.querySelector(".counter .correct");
+const incorrectElement = document.querySelector(".counter .incorrect");
+if (!correctElement || !incorrectElement) {
+  console.error('unable to find "correct" or "incorrect" element');
+}
+
+function questionAnswerHandler(e) {
+  const button = e.target;
+
+  const isCorrect = button.classList.contains("button-correct");
+
+  if (isCorrect) {
+  }
+}
+
+const buttons = document.querySelector("button");
+
+console.log(buttons);
+
+for (const button of buttons) {
+  button.addEventListener("click", questionAnswerHandler);
+}
+
+export function generateIndexHtml() {
+  const html = /* html */ `
+        <html>
+            <head>
+                <script>
+            </head>
+            
+    `;
+
+  return html;
+}
